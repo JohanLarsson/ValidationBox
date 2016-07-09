23e9c508d58ea27c5e38e3fab55b556f235b16f2
Johan Larsson <johan_gavle1@hotmail.com>
2016-07-09 22:11:45 +0200

    wip

---
+++ ValidationBox/FirstValidationErrorMessage.cs
@@ -0,0 +1,70 @@
+﻿namespace ValidationBox
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Markup;
+    using System.Xaml;
+
+    [MarkupExtensionReturnType(typeof(BindingExpression))]
+    public class FirstValidationErrorMessageForExtension : MarkupExtension
+    {
+        private static readonly PropertyPath ErrorCountPath = new PropertyPath("Validation.Errors.Count");
+
+        public FirstValidationErrorMessageForExtension(string elementName)
+        {
+            this.ElementName = elementName;
+        }
+
+        public string ElementName { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var resolver = (IXamlNameResolver)serviceProvider.GetService(typeof(IXamlNameResolver));
+            var source = resolver.Resolve(this.ElementName);
+            var binding = new Binding
+            {
+                Path = ErrorCountPath,
+                Converter = FirsteErrorConverter.Default,
+                ConverterParameter = source,
+                Source = source
+            };
+            return binding.ProvideValue(serviceProvider);
+        }
+
+        private class FirsteErrorConverter : IValueConverter
+        {
+            public static readonly FirsteErrorConverter Default = new FirsteErrorConverter();
+
+            private FirsteErrorConverter()
+            {
+            }
+
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                var element = (UIElement)parameter;
+                var errors = Validation.GetErrors(element);
+                if ((errors?.Count ?? 0) == 0)
+                {
+                    return string.Empty;
+                }
+
+                var first = errors[0];
+                var result = first.ErrorContent as ValidationResult;
+                if (result != null)
+                {
+                    return result;
+                }
+
+                return first.ErrorContent;
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
