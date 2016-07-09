9bf967c1cddc7c2bbe3922b5427d35a7ec464dae
Johan Larsson <johan_gavle1@hotmail.com>
2016-07-09 21:27:14 +0200

    Explicit fetch errors button.

--- ValidationBox/MainWindow.xaml.cs
+++ ValidationBox/MainWindow.xaml.cs
@@ -1,12 +1,20 @@
 ﻿namespace ValidationBox
 {
     using System.Windows;
+    using System.Windows.Controls;
 
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+        }
+
+        private void OnFetchErrorClick(object sender, RoutedEventArgs e)
+        {
+            var errors = Validation.GetErrors(this.TextBox);
+            this.ErrorBlock.Text = FirstErrorOrDefaultConverter.Default.Convert(errors, null, null, null)
+                                                               .ToString();
         }
     }
 }
