1cc1346e4cf78250cfa214c1a045493222e45e36
Johan Larsson <johan_gavle1@hotmail.com>
2016-07-09 21:31:42 +0200

    Xaml only fix.

---
+++ ValidationBox/FirstErrorForElementMultiValueConverter.cs
@@ -0,0 +1,24 @@
+﻿namespace ValidationBox
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+
+    public class FirstErrorForElementMultiValueConverter : IMultiValueConverter
+    {
+        public static readonly FirstErrorForElementMultiValueConverter Default = new FirstErrorForElementMultiValueConverter();
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var errors = Validation.GetErrors((DependencyObject)values[0]);
+            return FirstErrorOrDefaultConverter.Default.Convert(errors, null, null, null);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
